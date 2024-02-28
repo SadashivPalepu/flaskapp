cf9d27d9322a96cc6cd320a2334233045e461e84
SadashivPalepu <70357572+SadashivPalepu@users.noreply.github.com>
2024-02-28 15:46:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,6 @@ app =flask.Flask(__name__)
 @app.route("/")
 def show():
     return 'welcome to flask'
-
-@app.route("/user/<name>")
-def show1(name):
-    return f'<h1>welcome to {name}</h1>'
-
+    
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0")
+    app.run()
